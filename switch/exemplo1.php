5aa794b504ee2a7c1bb5e14570b0c734ce2232b4
Júlia <juliamalta12@outlook.com>
2020-05-05 16:20:48 -0300

    switch

---
+++ switch/exemplo1.php
@@ -0,0 +1,42 @@
+<?php
+
+$diaSemana = 8;//date ("w");
+
+switch ($diaSemana) {
+	case  0:
+echo"domingo";
+
+		break;
+
+case 1:
+echo"segunda";
+break;
+case 2:
+echo"terça";
+break;
+case 3:
+echo"quarta";
+break;
+case 4:
+echo"quinta";
+break;
+case 5:
+echo"sexta";
+break;
+
+case 6:
+echo"sabado";
+break;
+
+
+default:
+echo"data invalida";
+break;
+
+}
+
+
+
+
+
+?>
